1df2388cd6bb267d2b3ef96a57d8be1eb99c5e95
Victor Rodrigues <garevictor@hotmail.com>
2023-05-23 20:08:19 +0100

    Added vercel insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import Layout from '@/layouts/layout'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
             <Layout>
                 <Component {...pageProps} />
+                <Analytics />
             </Layout>
         </>
     )
