b7bb768c9e772eb70690af36ad5c76774ba067da
Victor Rodrigues <garevictor@hotmail.com>
2023-06-14 16:49:57 +0100

    Remvoved unecessary var

--- src/components/GuildInfo/GuildInfo.tsx
+++ src/components/GuildInfo/GuildInfo.tsx
@@ -37,7 +37,6 @@ export default function GuildInfo(props: GuildProps) {
 
     return (
         <>
-            {props.members.length}
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <MembersResidenceTable residences={membersResidence} />
             </div>
